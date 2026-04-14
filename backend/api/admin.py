e5c0f798cc09b4fce93c964d3fd629195ea77be5
Ilyas-50 <i_yelubayev@kkbtu.kz>
2026-04-14 20:53:16 +0500

    views, serial, urls

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Chat, ChatMember, Message
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'is_online')
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'type', 'created_at')
+
+@admin.register(ChatMember)
+class ChatMemberAdmin(admin.ModelAdmin):
+    list_display = ('chat', 'user', 'is_admin')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'chat', 'created_at')
+    readonly_fields = ('created_at',)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
+    # 'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,6 +54,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
 ]
 
 ROOT_URLCONF = "core.urls"
